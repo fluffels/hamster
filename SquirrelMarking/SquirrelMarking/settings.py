f9532808e3143b5d515bff54071bc9081eb3ab70
Gerard Lamusse <u12206050@tuks.co.za>
2014-04-16 23:28:07 +0200

    Merging..

--- SquirrelMarking/SquirrelMarking/settings.py
+++ SquirrelMarking/SquirrelMarking/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = ''
 STATICFILES_DIRS = (
 	"static",
 	)
--- SquirrelMarking/SquirrelMarking/urls.py
+++ SquirrelMarking/SquirrelMarking/urls.py
@@ -4,7 +4,7 @@ from views import *
 urlpatterns = patterns('',
 	#home page
 	(r'^$', loginWeb),
-	(r'^viewAssessments/$', viewAssessments),
+	(r'^getAssessments/$', viewAssessments),
 	#student report page
 	#~ (r'^studentReport/$', studentReport),
 	#~ #audit report page
@@ -12,42 +12,42 @@ urlpatterns = patterns('',
 	#~ #marker home page
 	#~ (r'^marker/$', marker_home),
 	#assessments view
-	#~ (r'^assessmentView/$', assessment_view),
+	(r'^assessmentView/$', assessment_view),
 	#manage assessments
-	#~ (r'^assessmentManager/$', assessment_manager),
-	#~ #manage sessions
-	#~ (r'^sessionManager/$', session_manager),
-	#~ #view audit report
-	#~ (r'^auditReport/$', view_audit_report),
-	#~ #reporting mani menu
-	#~ (r'^Reporting_Main/$', reporting_main),
-	#~ #statistics
-	#~ (r'^Statistics/$', statistics),
-	#~ #all students
-	#~ (r'^students/$', view_all_students),
-	#~ #view individual student
-	#~ (r'^student/(?P<studentNumber>\w{1}\d{8})/$', view_student),
-	#~ (r'^studentReport/$', student_report),
-	#~ (r'^unpublish/$', unpublish),
-	#~ (r'^marks-management/$', marks_management),
-	#~ #view individual course
-	#~ (r'^course/(?P<courseCode>\w{3}\d{3})/$', view_course),
-	#~ #view all courses
-	#~ (r'^courses/$', view_all_course),
+	(r'^assessmentManager/$', assessment_manager),
+	#manage sessions
+	(r'^sessionManager/$', session_manager),
+	#view audit report
+	(r'^auditReport/$', view_audit_report),
+	#reporting mani menu
+	(r'^Reporting_Main/$', reporting_main),
+	#statistics
+	(r'^Statistics/$', statistics),
+	#all students
+	(r'^students/$', view_all_students),
+	#view individual student
+	(r'^student/(?P<studentNumber>\w{1}\d{8})/$', view_student),
+	(r'^studentReport/$', student_report),
+	(r'^unpublish/$', unpublish),
+	(r'^marks-management/$', marks_management),
+	#view individual course
+	(r'^course/(?P<courseCode>\w{3}\d{3})/$', view_course),
+	#view all courses
+	(r'^courses/$', view_all_course),
 	
-	#~ (r'^auditReport/$', audit_report),
-	#~ (r'^Reporting_Main/$', reporting_main),
-	#~ (r'^Statistics/$', statistics),
-	#~ #(r'^studentChosen/$', student_chosen),
-	#~ #(r'^studentReport/$', student_report),
-	#~ (r'^unpublish/$', unpublish),
-	#~ (r'^marks-management/$', marks_management),
-	#~ (r'frequency_analysis/$', frequency_analysis),
-	#~ #(r'get_module_mark/$', get_module_mark),
-	#~ (r'getAssessments/$', getAssessments),
-	#~ (r'getLeafAssessments/$', getLeafAssessments),
-	#~ (r'getLecturerModules/$', getLecturerModules),
-	#~ (r'studentModules/$', studentModules),
-	#~ (r'searchStudents/$', searchStudents),
-	#~ (r'displayStudent/$', displayStudent),
+	(r'^auditReport/$', audit_report),
+	(r'^Reporting_Main/$', reporting_main),
+	(r'^Statistics/$', statistics),
+	#(r'^studentChosen/$', student_chosen),
+	#(r'^studentReport/$', student_report),
+	(r'^unpublish/$', unpublish),
+	(r'^marks-management/$', marks_management),
+	(r'frequency_analysis/$', frequency_analysis),
+	#(r'get_module_mark/$', get_module_mark),
+	(r'getAssessments/$', getAssessments),
+	(r'getLeafAssessments/$', getLeafAssessments),
+	(r'getLecturerModules/$', getLecturerModules),
+	(r'studentModules/$', studentModules),
+	(r'searchStudents/$', searchStudents),
+	(r'displayStudent/$', displayStudent),
 )
