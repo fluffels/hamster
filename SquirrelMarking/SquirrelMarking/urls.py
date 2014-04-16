1db84eb5a322f01707d91fd7f9172b55249eb52b
root <root@CSImage2013.cs.up.ac.za>
2014-04-16 17:27:01 +0200

    Web app yeah

--- SquirrelMarking/SquirrelMarking/urls.py
+++ SquirrelMarking/SquirrelMarking/urls.py
@@ -1,22 +1,53 @@
 from django.conf.urls import patterns, include, url
-from SquirrelMarking.views import *
+from views import *
 
 urlpatterns = patterns('',
-	(r'^$', login),
-	(r'^student/$', student_home),
-	(r'^lecturer/$', lecturer_home),
-	(r'^marker/$', marker_home),
-	(r'^assessmentView/$', assessment_view),
-	(r'^assessmentManager/$', assessment_manager),
-	(r'^sessionManager/$', session_manager),
+	#home page
+	(r'^$', loginWeb),
+	(r'^viewAssessments/$', viewAssessments),
+	#student report page
+	#~ (r'^studentReport/$', studentReport),
+	#~ #audit report page
+	#~ (r'^auditReport/$', auditReport),
+	#~ #marker home page
+	#~ (r'^marker/$', marker_home),
+	#assessments view
+	#~ (r'^assessmentView/$', assessment_view),
+	#manage assessments
+	#~ (r'^assessmentManager/$', assessment_manager),
+	#~ #manage sessions
+	#~ (r'^sessionManager/$', session_manager),
+	#~ #view audit report
+	#~ (r'^auditReport/$', view_audit_report),
+	#~ #reporting mani menu
+	#~ (r'^Reporting_Main/$', reporting_main),
+	#~ #statistics
+	#~ (r'^Statistics/$', statistics),
+	#~ #all students
+	#~ (r'^students/$', view_all_students),
+	#~ #view individual student
+	#~ (r'^student/(?P<studentNumber>\w{1}\d{8})/$', view_student),
+	#~ (r'^studentReport/$', student_report),
+	#~ (r'^unpublish/$', unpublish),
+	#~ (r'^marks-management/$', marks_management),
+	#~ #view individual course
+	#~ (r'^course/(?P<courseCode>\w{3}\d{3})/$', view_course),
+	#~ #view all courses
+	#~ (r'^courses/$', view_all_course),
 	
-	(r'^auditReport/$', audit_report),
-	(r'^Reporting_Main/$', reporting_main),
-	(r'^Statistics/$', statistics),
-	(r'^studentChosen/$', student_chosen),
-	(r'^studentReport/$', student_report),
-	(r'^unpublish/$', unpublish),
-	(r'^marks-management/$', marks_management),
-        (r'^checkLogin/$', user_login),
-        (r'^test/$', test),
+	#~ (r'^auditReport/$', audit_report),
+	#~ (r'^Reporting_Main/$', reporting_main),
+	#~ (r'^Statistics/$', statistics),
+	#~ #(r'^studentChosen/$', student_chosen),
+	#~ #(r'^studentReport/$', student_report),
+	#~ (r'^unpublish/$', unpublish),
+	#~ (r'^marks-management/$', marks_management),
+	#~ (r'frequency_analysis/$', frequency_analysis),
+	#~ #(r'get_module_mark/$', get_module_mark),
+	#~ (r'getAssessments/$', getAssessments),
+	#~ (r'getLeafAssessments/$', getLeafAssessments),
+	#~ (r'getLecturerModules/$', getLecturerModules),
+	#~ (r'studentModules/$', studentModules),
+	#~ (r'searchStudents/$', searchStudents),
+	#~ (r'displayStudent/$', displayStudent),
 )
--- SquirrelMarking/SquirrelMarking/views.py
+++ SquirrelMarking/SquirrelMarking/views.py
@@ -1,60 +1,98 @@
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_protect
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
 from django.template import Context
+from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login
-from dbModels.models import *
-from ldapView import *
 from businessLogicAPI import *
+from forms import *
+from django.shortcuts import render
+#from django.http import HttpResponseRedirect
 
-def test(request):
-    x2 = insertModule("COS301")
-    x3 = insertAssessment('test','asdas',"type",x2)
-    x4 = insertSessions("test",x3,'2012-12-12 12:12','2015-12-12 12:12',1)
-    xx = insertMarkSession("u89000999",x4)
-    x5 = insertMarkerModule("u89000999",x2)
-    x6 = insertLeafAssessment("name",x3,100,True)
-    x7 = insertMarkAllocation(x6,x3,20,x4,"asdsad","sadasd",'2012-12-12 12:12')
-    x8 = insertMarkerModule("asdasd",x2)
-    return HttpResponse("<html><body><p>"+str(len(getOpenSessions(2)))+"</p><p>"+str(getSessions()[0].assessment_id_id)+"</p></body></html>")
+@csrf_protect
+def loginWeb(request):
+	nform = LoginForm() 
+	if request.method == 'POST': # If the form has been submitted...		
+		# ContactForm was defined in the the previous section
+		form = LoginForm(request.POST) # A form bound to the POST data
+		if form.is_valid(): # All validation rules pass
+		# Process the data in form.cleaned_data
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			try:
+				login(request,username,password)
+				try:
+					P = getSessionPerson(request)
+					SOCourses = P.studentOf
+					TOCourses = P.tutorOf
+					TAOCourses = P.teachingAssistantOf
+					LOCourses = P.lectureOf
+					
+					#~ SOlist = []
+					#~ for mod in SOCourses:
+						#~ y = getAllSessionsForModule(mod)
+						#~ SOlist.append(y)
+						
+					#~ TOlist = []
+					#~ for mod in TOCourses:
+						#~ y = getAllSessionsForModule(mod)
+						#~ TOlist.append(y)
 
-def login(request):
-	t = get_template('login.html')
-	html = t.render(Context())
-	return HttpResponse(html)
+					#~ TAOlist = []
+					#~ for mod in TAOCourses:
+						#~ y = getAllSessionsForModule(mod)
+						#~ TAOlist.append(y)
 
-def student_home(request):
-	t = get_template('student.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-	
-def lecturer_home(request):
-	t = get_template('lecturer.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-	
-def marker_home(request):
-	t = get_template('marker.html')
-	html = t.render(Context())
-	return HttpResponse(html)
+					#~ LOlist = []
+					#~ for mod in LOCourses:
+						#~ y = getAllSessionsForModule(mod)
+						#~ LOlist.append(y)	
+					
+					print (P.firstName)
+					return render(request,'index.html', {'person': P})#, 'studentOf':SOlist, 'tutorOf':TOlist, 'teachingAssistantOf':TAOlist, 'lectureOf':LOlist}) # Redirect after POST
+				except Exception, e:
+					print (e)
+					return render(request,'login.html', {'form': nform, 'msg':"Session ERROR"})					
+			except Exception:
+				return render(request,'login.html', {'form': nform, 'msg':"Invalid details entered"})
+		else:
+			return render(request,'login.html', {'form': nform, 'msg':form.errors})#form.errors
+	else:	
+		return render(request,'login.html', {'form': nform})
+		
+		
+def viewAssessments(request, mod_code):
+	try:
+		P = getSessionPerson(request)
+		Assessments = getAllAssessmentsForModule(mod_code)
+		return render(request,'div.html', {'Assessments': Assessments})		
+	except:
+		return render(request,'login.html', {'form': nform,  'msg':"Please login"})
+		
+'''
+    REPORTING FUNCTIONS
+'''
+# frequency analysis function to initialize variables	
+def frequency_analysis(request):
+        person = Person()
+        return render_to_response('studentChosen.html',{'per': person,} )
 
+def getLeafAssessments(request):
+        person = getSessionPerson(request)
+        assess_id = request.POST['assess_id']
+        x = getLeafAssessmentMarksOfAsssessmentForStudent(person.upId, assess_id)
+        return render_to_response( 'studentChosen.html', {'leafAssessmentList' : x})
+        
+def  getAssessments(request):
+        person = getPresonByID(request.POST['studentID'])
+        leaf = request.POST['leafAssessment']
+        leafAssesment = getLeafAssesment() #function does not exist, make it!!!!!
+        type = request.POST['type']
+        assessmentList = getAllAssessmentTotalsForStudent(person.empl_no, request.POST['mod_code'])
+        return render_to_response( 'studentChosen.html', {'per' : person, 'usrAllAssessments' : assessmentList, 'type' : type, 'leafAssesment' : leafAssesment })
 
-def assessment_view(request):
-	t = get_template('assessmentView.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-
-def assessment_manager(request):
-	t = get_template('assessmentManager.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-
-def session_manager(request):
-	t = get_template('sessionManager.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-	
-	
-	
 def audit_report(request):
 	t = get_template('auditReport.html')
 	html = t.render(Context())
@@ -64,70 +102,23 @@ def reporting_main(request):
 	t = get_template('Reporting_Main.html')
 	html = t.render(Context())
 	return HttpResponse(html)
-
-
-def statistics(request):
-	t = get_template('Statistics.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-
-def student_chosen(request):
-	t = get_template('studentChosen.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-
-def student_report(request):
-	t = get_template('studentReport.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-	
-	
-def unpublish(request):
-	t = get_template('unpublish.html')
-	html = t.render(Context())
-	return HttpResponse(html)
-
-def marks_management(request):
-        t = get_template('marks-management.html')
-	html = t.render(Context())
-	return HttpResponse(html)	
-	
-def user_login(request):
-    # Like before, obtain the context for the user's request.
-    context = RequestContext(request)
-    print >>sys.stderr, 'Goodbye, cruel world!'
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        username = request.POST['username']
-        password = request.POST['password']
-
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
-        user = authenticateUser(username, password)
-
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
-        if user is not None:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
-                login(request, user)
-                return HttpResponseRedirect('home.html')
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your Squirrel account is not active.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
-
-        # The request is not a HTTP POST, so display the login form.
-        # This scenario would most likely be a HTTP GET.
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        return render_to_response('login.html', {}, context)
+        
+def studentModules(request):
+        student = getSessionPerson(request)
+        render_to_response('studentChosen.html', {'modules' : student.getAllModulesForStudent()})        
+        
+def getLecturerModules(request):
+        lecturer = getSessionPerson(request)
+        return render_to_response('assessmentReport.html', {'modules' : getAllModulesForLecturer(lecturer.upId)})
+        
+def searchStudents(request):
+        searchedItemRequest = request.POST['searchedItem']
+        students = findPerson(searchedItemRequest) #function does not exist, make it!!!!!
+        render_to_response('studentChosen.html', {'students' : students})
+        
+def displayStudent(request):
+	person = getStudentByID(request.POST['upId'])
+	render_to_response('studentReport.html', {'person' : person})
+'''
+    END REPORTING FUNCTIONS
+'''
