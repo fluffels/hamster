8baf4e3224b39c83cce6df18926639f4d66ef201
Kay789 <siphokazi.manana@gmail.com>
2014-05-23 03:20:18 +0200

    Deleted files

--- SquirrelMarking/dbModels/sql/markermodule.sql
+++
@@ -1,3 +0,0 @@
-insert into dbModels_markermodule (marker_id,module_id) values ("u89000105","COS301");
-insert into dbModels_markermodule (marker_id,module_id) values ("u89000105","COS332");
-insert into dbModels_markermodule (marker_id,module_id) values ("u89000105","COS110");
