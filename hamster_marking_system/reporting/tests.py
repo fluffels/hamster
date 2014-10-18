686bacf6a8d69b68a2e35f714892cc4f80bf5e4a
Siphokazi u12064115 <siphokazi.manana@gmail.com>
2014-10-18 20:45:59 +0200

    Completing tests

--- hamster_marking_system/reporting/tests.py
+++ hamster_marking_system/reporting/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 import unittest
 from mock import MagicMock
 from reporting import views
-
+from reporting import reporting_api as rapi
 
 class testViews(unittest.TestCase):
     def test_get_assessment_report(self):
@@ -42,16 +42,39 @@ class testViews(unittest.TestCase):
         
 
 class ReportingAPITestCase(unittest.TestCase):
+       
     def test_generate_assessment_report(self):
-        pass
+        mock_request = MagicMock()
+        mock_render_to_responce = MagicMock()
+        
+        rapi.generate_assessment_report = MagicMock(return_value = mock_render_to_responce)
+        rapi.generate_assessment_report(mock_request)
+        rapi.generate_assessment_report.assert_called_once_with(mock_request)
+        rapi.generate_assessment_report.assert_return_value(mock_render_to_responce)
     
     def test_generate_student_mark_pdf(self):
-        pass
+        mock_request = MagicMock()
+        mock_render_to_responce = MagicMock()
+        
+        rapi.generate_student_mark_pdf = MagicMock(return_value = mock_render_to_responce)
+        rapi.generate_student_mark_pdf(mock_request)
+        rapi.generate_student_mark_pdf.assert_called_once_with(mock_request)
+        rapi.generate_student_mark_pdf.assert_return_value(mock_render_to_responce)
     
     def test_generate_student_mark_csv(self):
-        pass
+        mock_request = MagicMock()
+        mock_render_to_responce = MagicMock()
+        
+        rapi.generate_student_mark_csv = MagicMock(return_value = mock_render_to_responce)
+        rapi.generate_student_mark_csv(mock_request)
+        rapi.generate_student_mark_csv.assert_called_once_with(mock_request)
+        rapi.generate_student_mark_csv.assert_return_value(mock_render_to_responce)
     
     def test_read_from_csv_file(self):
-        pass
+        mock_request = MagicMock()
+        mock_render_to_responce = MagicMock()
         
-        
+        rapi.read_from_csv_file = MagicMock(return_value = mock_render_to_responce)
+        rapi.read_from_csv_file(mock_request)
+        rapi.read_from_csv_file.assert_called_once_with(mock_request)
+        rapi.read_from_csv_file.assert_return_value(mock_render_to_responce)    
